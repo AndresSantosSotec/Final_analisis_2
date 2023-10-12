df8d56e7f1734977229f8704853d3ad17d9cb194
SpeedDevloperAndres <andressantostkd@gmail.com>
2023-10-11 20:43:49 -0600

    cambios ,esta en pañales

--- landing/HomePage.php
+++ landing/HomePage.php
@@ -25,60 +25,70 @@
 
   <body>
 
-    <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
-      <div class="container-fluid">
-
+  <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+    <div class="container-fluid">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-          data-bs-target="#navbarColor01" aria-controls="navbarColor01"
-          aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+            data-bs-target="#navbarColor01" aria-controls="navbarColor01"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
         </button>
         <li class="nav-item">
             <img src="../assets/images/logo.png" alt="Logo" style="width: 120px; height: 120px;">
-          </li>
+        </li>
         <div class="collapse navbar-collapse" id="navbarColor01">
-
-
-          <ul class="navbar-nav me-auto">
-            
-            <li class="nav-item">
-              <a class="nav-link active" href="#">Inicio
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Servicios</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Mas de Nosotros</a>
-            </li>
-          </ul>
-
-          <ul class="navbar-nav me-auto">
-
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown"
-                role="button"
-                data-bs-toggle="dropdown" aria-expanded="false">
-                Aun no eres CLiente?
-              </a>
-              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <ul class="navbar-nav me-auto">
                 <li class="nav-item">
-                  <a class="nav-link" href="#">
-                    <i class="fas fa-pencil-alt"></i> Registarse
-                  </a>
+                    <a class="nav-link active" href="#">Inicio</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">
-                    <i class="fas fa-user"></i> Iniciar Sesión
-                  </a>
+                    <a class="nav-link" href="#">Servicios</a>
                 </li>
-              </ul>
-            </li>
-
-          </ul>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Mas de Nosotros</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown"
+                        role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Aun no eres CLiente?
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <i class="fas fa-pencil-alt"></i> Registarse
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">
+                                <i class="fas fa-user"></i> Iniciar Sesión
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+            <!-- Shopping Cart Icon -->
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="../Vistas/Tienda.php">
+                        <i class="fas fa-shopping-cart"></i> Visita nuestra tienda
+                    </a>
+                </li>
+            </ul>
         </div>
+    </div>
+</nav>
+
+
+    <div class="card">
+      <div class="card-body">
+        This is some text within a card body.
       </div>
-    </nav>
+    </div>
+
+    
+
+
 
     
 
